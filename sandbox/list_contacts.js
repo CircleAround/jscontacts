0b2901290df5b59dd34c67f954527cb563fd9a3b
Masashi Sato <ms2.sato@gmail.com>
2022-01-14 17:06:03 +0900

    first migration

---
+++ sandbox/list_contacts.js
@@ -0,0 +1,9 @@
+const models = require('../models');
+
+async function listContacts(){
+  const contacts = await models.Contact.findAll();
+  // console.log(contacts);
+  console.log(contacts.map((contact) => { return contact.name; }))
+}
+
+listContacts();
