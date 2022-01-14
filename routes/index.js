732e848f9936f5f394679c8d3f3b6d22d8eac837
Masashi Sato <ms2.sato@gmail.com>
2022-01-14 14:55:30 +0900

    about page

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello World', now: now });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
 module.exports = router;
