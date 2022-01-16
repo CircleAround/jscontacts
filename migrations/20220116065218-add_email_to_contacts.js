0d3998c7badaaa610a013b0dff04e88b99ba5f2c
Masashi Sato <ms2.sato@gmail.com>
2022-01-16 19:08:07 +0900

    add email column to Contacts

---
+++ migrations/20220116065218-add_email_to_contacts.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn(
+      'Contacts',
+      'email',
+      Sequelize.STRING
+    );    
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn(
+      'Contacts',
+      'email'
+    );
+  }
+};
