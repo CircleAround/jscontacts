fa6d974a3f5b6c2bdb8e04d9d1bdca342e567d04
Masashi Sato <ms2.sato@gmail.com>
2022-01-16 19:43:23 +0900

    add inserting from Web

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,8 +16,10 @@ router.get('/contact_form', function(req, res, next) {
   res.render('contact_form', { title: '連絡先フォーム' });
 });
 
-router.post('/contacts', function(req, res, next) {
+router.post('/contacts', async function(req, res, next) {
   console.log('posted', req.body);
+  const contact = models.Contact.build({ name: req.body.name, email: req.body.email });
+  await contact.save();
   res.redirect('/');
 });
 
